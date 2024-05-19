c2d36d17cefbc7cbb0686112754eacedf6a28a9a
EunHakKim <acky529@gmail.com>
2024-05-20 04:23:44 +0900

    [feat] 공격 기능 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,34 +5,117 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public Weapon weapon;
+
     float hAxis;
     float vAxis;
-    bool wDown;
+
+    bool rDown;
+    bool jDown;
+    bool fDown;
+
+    bool isJump;
+    bool isDodge;
+    bool isFireReady;
 
     Vector3 moveVec;
+    Vector3 dodgeVec;
 
+    Rigidbody rigid;
     Animator anim;
+
+    float fireDelay;
 
     // Start is called before the first frame update
     void Start()
     {
+        rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+        Attack();
+        Dodge();
+    }
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
-        wDown = Input.GetButton("Walk");
+        rDown = Input.GetButton("Run");
+        jDown = Input.GetButtonDown("Jump");
+        fDown = Input.GetButtonDown("Fire1");
+    }
 
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-        transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
+        if(isDodge)
+            moveVec = dodgeVec;
 
-        anim.SetBool("isRun", moveVec != Vector3.zero);
-        anim.SetBool("isWalk", wDown);
+        transform.position += moveVec * speed * (rDown ? 1.3f : 1f) * Time.deltaTime;
 
+        anim.SetBool("isWalk", moveVec != Vector3.zero);
+        anim.SetBool("isRun", rDown);
+    }
+
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if(jDown && moveVec == Vector3.zero && !isJump && !isDodge) {
+            rigid.AddForce(Vector3.up * 15, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+
+    void Attack()
+    {
+        fireDelay += Time.deltaTime;
+        isFireReady = weapon.rate < fireDelay;
+
+        if(fDown && isFireReady && !isDodge) {
+            weapon.Use();
+            anim.SetTrigger("doShot");
+            fireDelay = 0;
+        }
+    }
+
+    void Dodge()
+    {
+        if(jDown && moveVec != Vector3.zero && !isJump && !isDodge) {
+            dodgeVec = moveVec;
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.5f);
+        }
+    }
+
+    void DodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor") {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
     }
 }
